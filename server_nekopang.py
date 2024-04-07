e82a5f6c25a0eca86ad755828534b4647a9f1163
myDdoDdomi <gumtteun@gmail.com>
2024-04-08 08:39:05 +0900

    test

--- server_nekopang.py
+++ server_nekopang.py
@@ -120,18 +120,18 @@ def start():
 
     
 
-def handle_client(client_socket, _):
-    global client_sockets, tri_ready, point_dict, point_fin, final_tri
+def handle_client(client_socket, _): # 클라이언트들 관리 함수 
+    global client_sockets, tri_ready, point_dict, point_fin, final_tri # global로 클라리언트 리스트 통해서 클라이언트 관리
     while tri_ready == 0 :
-        if tri_ready == 1 :
-            for client in client_sockets:
-                client.send("시작".encode('utf-8'))
+        if tri_ready == 1 : # 트리거 이용하여 조건 만족 시 클라이언트 들에게 시작 문자 send
+            for client in client_sockets: # for문 이용하여 리스튼 안 있던 모든 클라이언트들에게 메세지 send
+                client.send("시작".encode('utf-8')) 
                 
-    point = client_socket.recv(1024).decode('utf-8')
-    NAME, point = point.split()
-    point_dict[NAME] = int(point)
-    point_fin = sorted(point_dict.items(), key=lambda x: x[1], reverse=True)
-    point_fin = dict(point_fin)
+    point = client_socket.recv(1024).decode('utf-8') # 각 클라이언트들이 보내는 포인트 및 이름 메시지 recv 받기 메시지 유형은 f"{NAME} {point}""
+    NAME, point = point.split() # 스트링 각각 변수로 잘라줘서 젖아하기
+    point_dict[NAME] = int(point) # 딕셔너리 형태로 이름 각각에 담기
+    point_fin = sorted(point_dict.items(), key=lambda x: x[1], reverse=True)  # value 값으로 내림차순 정렬하기
+    point_fin = dict(point_fin) 
     final_tri = 1
         # else :
         #     tri_ready = 1
@@ -154,14 +154,14 @@ def main():
         print("Server started, listening on port", PORT)
         
         a = ready_neko()
-        a.game_start()
+        a.game_start() # 게임 화면이랑 클라이언트 관리 함수를 쓰레드 이용하여 따로 관리
         while True:
-            client_socket, _ = server_socket.accept()
+            client_socket, _ = server_socket.accept() # 클라이언트 서버에 연결 
             yaong[random.choice(range(7))].play()
-            client_sockets.append(client_socket)
+            client_sockets.append(client_socket)  # 연결한 클라이언트들 한꺼번에 관리하기 위해 리스트안에 클라이언트들 append 
             print("Client connected")
             print("참가자 수 : ", len(client_sockets))
-            start_new_thread(handle_client, (client_socket, _))
+            start_new_thread(handle_client, (client_socket, _)) # 순차적으로 들어온 클라이언트들 다음 함수로 쓰레드를 통해 각각 넣어줌
             
 
 if __name__ == "__main__":
